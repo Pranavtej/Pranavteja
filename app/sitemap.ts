e273c1af81065593c24bb489bb4602acc0749112
Pranav Teja <86045290+Pranavtej@users.noreply.github.com>
2024-03-30 19:22:29 +0530

    fix-errors

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,10 @@
-import { allPosts } from "contentlayer/generated";
+
 
 export default async function sitemap() {
-  const posts = allPosts.map((post) => ({
-    url: `https://steventey.com/blog/${post.slug}`,
-    lastModified: post.publishedAt,
-  }));
-
   const routes = ["", "/about", "/projects", "/blog"].map((route) => ({
-    url: `https://steventey.com${route}`,
+    url: `https://pranavteja.vercel.app${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...posts];
+  return [...routes];
 }
