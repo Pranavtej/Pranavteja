cc9bee63a50b1e3ad772db176d7ec963a35b70f3
Pranav Teja <86045290+Pranavtej@users.noreply.github.com>
2024-03-30 19:27:56 +0530

    resume-updated

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,7 +4,7 @@ export default function Resume() {
     return (
         <div className="mt-4 mx-5 sm:mx-0 flex flex-col space-y-5 max-w-xl">
             <iframe
-                src="https://pranavtej.github.io/Pranavteja/pranavtejaresume.pdf"
+                src="https://pranavtej.github.io/Pranavteja/pranavtejapathiresume.pdf"
                 width="1080"
                 height="1080"
                 title="Pranav's Resume"
