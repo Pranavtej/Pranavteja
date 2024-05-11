c55dda94f15f03db032875416690b4fc8e6c9dae
Pranav Teja <86045290+Pranavtej@users.noreply.github.com>
2024-05-11 18:51:21 +0530

    add-project

--- app/page.tsx
+++ app/page.tsx
@@ -32,27 +32,27 @@ export default function Home() {
         </a>
         , I contribute to the development of several outstanding products.
       </p>
-      {/* <p className="mt-6 text-base text-zinc-600">
+      <p className="mt-6 text-base text-zinc-600">
         I also maintain projects like{" "}
         <a
-          href="https://dub.sh/"
+          href="https://destination-changer.vercel.app"
           target="_blank"
           rel="noreferrer noopener"
           className="underline underline-offset-4 text-zinc-800 font-semibold"
         >
           destination-changer
         </a>
-        ,{" "}
-        <a
-          href="https://novel.sh/"
+        {/* ,{" "} */}
+        {/* <a
+          href="https:"
           target="_blank"
           rel="noreferrer noopener"
           className="underline underline-offset-4 text-zinc-800 font-semibold"
         >
           telugu-lang
-        </a>
+        </a> */}
         .
-      </p> */}
+      </p>
       <div className="mt-6 flex gap-6">
         <a
           href="https://twitter.com/impranavteja"
