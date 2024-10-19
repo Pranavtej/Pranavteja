e48289e35d269559bb34cc44aae7a3d02312b931
Pranav Teja <86045290+Pranavtej@users.noreply.github.com>
2024-10-19 13:36:53 +0530

    fix: tweet

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,10 +12,10 @@ export default async function Blog() {
   return (
     <div className="mx-5 sm:mx-auto mt-16 sm:mt-20 md:border-l md:border-zinc-100 md:pl-6">
       <div className="flex max-w-3xl flex-col space-y-16">
-
-        <p className="text-xl font-medium text-zinc-400">
+        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Started my <a href="https://twitter.com/hashtag/OpenSource?src=hash&amp;ref_src=twsrc%5Etfw">#OpenSource</a> contribution this year with <a href="https://twitter.com/unkeydev?ref_src=twsrc%5Etfw">@unkeydev</a> 🖤! <br><br>Big thanks to <a href="https://twitter.com/chronark_?ref_src=twsrc%5Etfw">@chronark_</a> <a href="https://twitter.com/hashtag/ossgg?src=hash&amp;ref_src=twsrc%5Etfw">#ossgg</a> <a href="https://twitter.com/hashtag/opensource?src=hash&amp;ref_src=twsrc%5Etfw">#opensource</a> <a href="https://t.co/HXDFuGSxcK">pic.twitter.com/HXDFuGSxcK</a></p>&mdash; Pranav Teja (@impranavteja) <a href="https://twitter.com/impranavteja/status/1847547014545846430?ref_src=twsrc%5Etfw">October 19, 2024</a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        {/* <p className="text-xl font-medium text-zinc-400">
          No posts yet. Check back later!
-        </p>
+        </p> */}
         {/* {allPosts
           .sort((a, b) => {
             if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
